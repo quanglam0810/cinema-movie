91bdf02aa4ce37ac383c6d0c44855a959d5964ea
quanglam0810 <77328074+quanglam0810@users.noreply.github.com>
2022-06-13 23:43:52 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ const express = require("express");
 
 const rootRouter = require("./src/routers");
 
-const {sequelize} = require('./models');
-const path  = require('path');
-const { logger } = require("./src/middlewares/logger");
+//const {sequelize} = require('./models');
+//const path  = require('path');
+//const { logger } = require("./src/middlewares/logger");
 
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/public', express.static(path.join(__dirname, 'public')));
+//app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use(logger);
+//app.use(logger);
 
 const PORT = process.env.PORT || 3000;
 
 app.use('/api/v1',rootRouter );
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log("Connection has been established successfully.");
-  })
-  .catch((err) => {
-    console.error("Unable to connect to the database:", err);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log("Connection has been established successfully.");
+//   })
+//   .catch((err) => {
+//     console.error("Unable to connect to the database:", err);
+//   });
 
 app.listen(PORT, ()=>{
     console.log("app runing...");
