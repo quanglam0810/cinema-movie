7416f883197c323367d31f0102fc3a071ca134b5
quanglam0810 <77328074+quanglam0810@users.noreply.github.com>
2022-06-14 00:25:26 +0700

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -3,21 +3,21 @@ const express = require('express');
 
 const quanLyPhimRouter = require('./quanLyPhim');
 
-const quanLyNguoiDungRouter = require('./quanLyNguoiDung');
+//const quanLyNguoiDungRouter = require('./quanLyNguoiDung');
 
 const quanLyRapRouter = require('./quanLyRap');
 
-const quanLyDatVeRouter = require('./quanLyDatVe');
+//const quanLyDatVeRouter = require('./quanLyDatVe');
 
 const rootRouter = express.Router();
 
 rootRouter.use('/quanLyPhim', quanLyPhimRouter);
 
-rootRouter.use('/quanLyNguoiDung', quanLyNguoiDungRouter);
+//rootRouter.use('/quanLyNguoiDung', quanLyNguoiDungRouter);
 
 rootRouter.use('/quanLyRap', quanLyRapRouter);
 
-rootRouter.use('/quanLyDatVe', quanLyDatVeRouter);
+//rootRouter.use('/quanLyDatVe', quanLyDatVeRouter);
 
 
 rootRouter.get('/', (req, res)=>{
